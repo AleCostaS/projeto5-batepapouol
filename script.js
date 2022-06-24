35f600bea0e5b5a63119955175ece338e616faf7
AleCostaS <alexandrec.silva@usp.br>
2022-06-24 20:18:19 -0300

    colocando os usuarios online e checando o status do usuario corretamente

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const resposta = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages').then(renderizarMensagens).catch(retornarErro);
+let resposta = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages').then(renderizarMensagens).catch(retornarErro);
+let usuarios = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants').then(renderizarUsuarios);
+let nome = "";
 
 function retornarErro(erro){
     console.log("Status code: " + erro.response.status);
@@ -7,10 +9,9 @@ function retornarErro(erro){
 
 function renderizarMensagens(resposta){
     const ul = document.querySelector(".conteudo");
-    ul.innerHTML += '<div></div><div></div><div></div>'
+    ul.innerHTML = '<div></div><div></div><div></div>'
     let filha;
     let name;
-
     for (let i = 1; i < (resposta.data).length; i++){
         ul.innerHTML += 
         `<div class="mensagem">
@@ -27,6 +28,7 @@ function renderizarMensagens(resposta){
         if (resposta.data[i].type == "status"){
             filha.classList.add("system")
         } else if (resposta.data[i].type == "private_message"){
+            filha.classList.add("private")
             name.innerHTML += ` reservadamente para ${resposta.data[i].to}:`
         } else {
             name.innerHTML += ` para ${resposta.data[i].to}:`
@@ -45,18 +47,15 @@ function mudarPagina(){
 function digitandoNome(){
     const input = document.querySelector(".inicial input");
     if (input.value != ""){
-        let nome = 
+        nome = 
             {
                 name: input.value
             }
 
-        const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , nome).then(mudarPagina).catch(retornarErro);
-        setInterval(manterConexao(nome), 5000);
+        axios.post("https://mock-api.driven.com.br/api/v6/uol/participants" , nome).then(mudarPagina).catch(retornarErro);
+        axios.get('https://mock-api.driven.com.br/api/v6/uol/participants', nome).then(renderizarUsuarios);
+        return nome;
     }
-}
-
-function manterConexao(nome){
-    axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome).catch(perdeuConexao);
 }
 
 function perdeuConexao(erro){
@@ -68,12 +67,58 @@ function perdeuConexao(erro){
 function menuLateral(){
     const menu = document.querySelector(".menu");
     menu.classList.add("visivel");
+}
+
+function renderizarUsuarios(resposta){
     const menuLateral = document.querySelector(".menulateral")
-    menuLateral.innerHTML += `
+    menuLateral.innerHTML = `
     <p><strong>Escolha um contato para enviar mensagem:</strong></p>
     <div class="usuarios">
-        <div class="icone"><ion-icon name="people" onclick="menuLateral()"></ion-icon> </div>
-        <div class="nomeUsuarios">Todos</div>
+        <div class="icone"><ion-icon name="people"></ion-icon> </div>
+        <div class="nomeUsuarios selecionado">Todos</div>
+        <div class="escolhido"><ion-icon class name="checkmark-outline"></ion-icon></div>
     </div>
     `
+
+    for (let i = 1; i < (resposta.data).length; i++){
+        menuLateral.innerHTML += `
+        <div class="usuarios">
+            <div class="icone"><ion-icon name="person-circle-outline"></ion-icon> </div>
+            <div class="nomeUsuarios">${resposta.data[i].name}</div>
+        </div>
+        `
+    }
+
+    menuLateral.innerHTML += `
+    <p><strong>Escolha a visibilidade:</strong></p>
+    <div class="usuarios">
+        <div class="icone"><ion-icon name="lock-open"></ion-icon> </div>
+        <div class="nomeUsuarios">Público</div>
+    </div>
+    <div class="usuarios">
+        <div class="icone"><ion-icon name="lock-closed"></ion-icon> </div>
+        <div class="nomeUsuarios">Reservadamente</div>
+    </div>
+    `
+}
+
+function atualizarStatus(nome){ 
+    if (nome != ""){
+         axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nome).catch(perdeuConexao); 
+    }
+}
+
+setInterval(atualizarStatus(nome) , 5000);
+
+function atualizandoUsuarios(){ 
+    usuarios = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants').then(renderizarUsuarios);
+}
+
+setInterval( atualizandoUsuarios, 10000);
+
+function voltar(){
+    const menu = document.querySelector(".menu");
+    menu.classList.remove("visivel");
+    menu.classList.add("invisivel");
+    setTimeout(function() { menu.classList.remove("invisivel"); }, 1000);
 }
